edb013e8a021a2cbab4c3ca8d7a7b7038d9bb97c
Whale Mo <mo@juhuiwan.cn>
2016-05-13 17:04:57 +0800

    Add speed test file.

---
+++ index.js
@@ -0,0 +1,10 @@
+const fs = require('fs'),
+      path = require('path');
+
+var Router = module.exports = function (options = {}){
+    // Build route tree from root dir.
+
+    // TODO: update to async/await when node support it.
+    return function *(){
+    };
+}
---
+++ test/route_speed_test.js
@@ -0,0 +1,11 @@
+describe( 'Route Switch',  () => {
+    const should = require('should');
+
+    describe ( 'Normal regex test path', done => {
+        
+    });
+
+    describe ( 'This router way', done => {
+        
+    });
+});
